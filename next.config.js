dab19262e3107bc33c1ac539d73103f97ab1b15c
Jeanne Hua <x25hua@uwaterloo.ca>
2022-05-17 19:40:19 -0400

    ??

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OPENAI_SECRET: process.env.OPENAI_SECRET
+  }
 }
 
 module.exports = nextConfig
